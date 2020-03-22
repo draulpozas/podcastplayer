7ff7b8474a80e1c7987381c68d23cbb52917f6f3
Diego Pozas <draulpozas@gmail.com>
2020-03-23 00:21:17 +0100

    modified init.sql

--- db/sql/init.sql
+++ db/sql/init.sql
@@ -39,28 +39,8 @@ CREATE TABLE IF NOT EXISTS `subscription` (
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
---
--- Volcado de datos para la tabla `subscription`
---
-
-INSERT INTO `subscription` (`id`, `user_id`, `feed`) VALUES
-(1, 1, 'http://www.ivoox.com/puzolana-f1_fg_f1669469_filtro_1.xml'),
-(2, 1, 'http://www.ivoox.com/sinapsis_fg_f1582681_filtro_1.xml'),
-(3, 1, 'https://www.relay.fm/cortex/feed'),
-(4, 1, 'https://www.relay.fm/liftoff/feed'),
-(5, 1, '../../testRssFiles/hellointernet.xml'),
-(7, 1, 'https://www.ivoox.com/podcast-a-cola-del-peloton_fg_f1136710_filtro_1.xml'),
-(8, 1, 'https://www.relay.fm/presentable/feed'),
-(9, 1, 'https://www.relay.fm/upgrade/feed'),
-(10, 2, ''),
-(11, 2, 'https://www.relay.fm/testdrivers/feed'),
-(12, 2, 'https://www.relay.fm/testdrivers/feed'),
-(13, 2, 'https://www.relay.fm/testdrivers/feed'),
-(14, 1, 'https://www.relay.fm/testdrivers/feed'),
-(15, 1, 'jfdjdfjdfjdf');
 
 -- --------------------------------------------------------
-
 --
 -- Estructura de tabla para la tabla `user`
 --
@@ -73,18 +53,6 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(256) COLLATE latin1_spanish_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
-
---
--- Volcado de datos para la tabla `user`
---
-
-INSERT INTO `user` (`id`, `username`, `passwd`, `email`) VALUES
-(1, 'diego', '$2y$10$SgNwfjsnOB41MTJOXUTA0ep6zy0gGjbkmqoNEg7s5Ykv3kyE3UoR2', 'dipo.9913@gmail.com'),
-(2, 'diego2', '$2y$10$LUe9bjrHaRso1HyQyYMi8uAAG3hlKJvrNgGh19y20g9i2ZSPWU/Vy', 'dipo.9913@gmail.com');
-
---
--- Restricciones para tablas volcadas
---
 
 --
 -- Filtros para la tabla `subscription`
